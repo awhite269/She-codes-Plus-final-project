4ffd72bc65bd52fb37f61d3bd863234482e076ed
awhite269 <131501883+awhite269@users.noreply.github.com>
2023-06-11 17:31:47 -0400

    updated HTML(rearranged order of divs), CSS and JS

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-let now = new Date();
-let h2 = document.querySelector("h2");
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-let month = months[now.getMonth()];
-let date = now.getDate();
-let year = now.getFullYear();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let meridiem = "AM";
-if (hours > 12) {
-  hours = hours - 12;
-  meridiem = "PM";
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+  let h1 = document.querySelector("h1");
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[now.getMonth()];
+  let date = now.getDate();
+  let year = now.getFullYear();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let meridiem = "AM";
+  if (hours > 12) {
+    hours = hours - 12;
+    meridiem = "PM";
+  }
+  h1.innerHTML = `${month} ${date}, ${year} ${hours}:${minutes} ${meridiem}`;
+  return `${month} ${date}, ${year} ${hours}:${minutes} ${meridiem}`;
 }
-h2.innerHTML = `${month} ${date}, ${year} ${hours}:${minutes} ${meridiem}`;
+formatDate(new Date());
 
 function displayWeatherCondition(response) {
-  document.querySelector("h1").innerHTML = response.data.name;
-  document.querySelector("h3").innerHTML = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#temperature");
+  document.querySelector.innerHTML = response.data.city;
+  document.querySelector.innerHTML = Math.round(
+    response.data.temperature.current
+  );
+
   let cityElement = document.querySelector("#city");
+  let dateElement = document.querySelector("#date");
+  let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.temperature.current;
@@ -54,6 +61,12 @@ function searchCity(city) {
   let apiKey = "53ab2140f215ee6a91f254aoaedc8t9d";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-input").value;
+  searchCity(city);
 }
 
 let searchForm = document.querySelector("#search-form");
@@ -79,13 +92,10 @@ function displayCelsiusTemperature(event) {
 
 let celsiusTemperature = null;
 
-let form = document.querySelector("#search-input");
-form.addEventListener("submit", handleSubmit);
+// let fahrenheitLink = document.querySelector("#fahrenheit-link");
+// fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+// let celsiusLink = document.querySelector("#celsius-link");
+// celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-search("Hull");
+searchCity("Null");
